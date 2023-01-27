f0c50a2949ff0c4efa54e5a5c1ad8637f8853a31
Alexey Baranov <Amaentes@icloud.com>
2023-01-27 20:19:13 +0300

    number1

---
+++ number1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите сначала число а, потом b");
+Console.ReadLine("a");
+Console.ReadLine("b");
+if(a > b);
+{
+    Console.WriteLine("a большее число, b меньшее число");
+}
+else
+{
+  Console.WriteLine("b большее число, a меньшее число");  
+}
