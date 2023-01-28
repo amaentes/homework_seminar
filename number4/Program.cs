87daecb7a80e4463e2c1aeaaa267772c9e407310
Alexey Baranov <Amaentes@icloud.com>
2023-01-28 14:01:09 +0300

    number4

---
+++ number4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int n;
+int i = 1;
+bool not = true;
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + n);
+while (i <= n)
+{
+  if (i % 2 != 1)
+{
+  Console.Write(i + ", ");
+  not = false;
+}
+i++;
+}
+if (not)
+{
+  Console.WriteLine("Нет чётных чисел!");
+}
