96f3fbb56715269a141d288b3b27f808a6fae181
Alexey Baranov <Amaentes@icloud.com>
2023-02-07 22:02:54 +0300

    number13

---
+++ number13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int lenArray = 8;
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(lenArray);
+    Console.Write(randomArray[i] + " ");
+}
+Console.WriteLine("");
+
